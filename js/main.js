d0c67ab7ef543c41846c7279b6512af10e089479
Akash Vaidya <akashisvaidya@gmail.com>
2023-05-16 23:18:23 +1000

    Merge unrelated histories

--- js/main.js
+++ js/main.js
@@ -27,48 +27,46 @@
 // houseThree();
 
 //Code 03
-function houseOne() {
-  console.log("Paper delivered to house 1");
-}
-function houseTwo(callback) {
-  setTimeout(() => {
-    console.log("Paper delivered to house 2");
-    callback();
-  }, 3000);
-}
-function houseThree() {
-  console.log("Paper delivered to house 3");
-}
-houseOne();
-houseTwo(houseThree);
+// function houseOne() {
+//   console.log("Paper delivered to house 1");
+// }
+// function houseTwo(callback) {
+//   setTimeout(() => {
+//     console.log("Paper delivered to house 2");
+//     callback();
+//   }, 3000);
+// }
+// function houseThree() {
+//   console.log("Paper delivered to house 3");
+// }
+// houseOne();
+// houseTwo(houseThree);
 
 //Code 04
-// function houseOne(){
+// function houseOne() {
+//   setTimeout(() => {
+//     console.log("Paper delivered to house 1");
 //     setTimeout(() => {
-//         console.log('Paper delivered to house 1')
-//         setTimeout(() => {
-//             console.log('Paper delivered to house 2')
-//             setTimeout(() => {
-//                 console.log('Paper delivered to house 3')
-//             }, 3000)
-//         }, 4000)
-//     }, 5000)
+//       console.log("Paper delivered to house 2");
+//       setTimeout(() => {
+//         console.log("Paper delivered to house 3");
+//       }, 3000);
+//     }, 4000);
+//   }, 5000);
 // }
-// houseOne()
+// houseOne();
 
 //Code 05
-// const promise = new Promise((resolve, reject) => {
-//     const error = false
-//     if(!error){
-//         resolve('Promise has been fullfilled')
-//     }else{
-//         reject('Error: Operation has failed')
-//     }
-// })
-// console.log(promise)
-// promise
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+const promise = new Promise((resolve, reject) => {
+  const error = false;
+  if (!error) {
+    resolve("Promise has been fullfilled");
+  } else {
+    reject("Error: Operation has failed");
+  }
+});
+console.log(promise);
+promise.then((data) => console.log(data)).catch((err) => console.log(err));
 
 //Code 06
 // function houseOne(){
